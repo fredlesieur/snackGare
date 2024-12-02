e115f064016caaaace00d4df102e40877e0969df
fredlesieur <fred.lesieur@hotmail.fr>
2024-12-02 19:25:41 +0100

    Ajout explicite du fichier Config/Db.php

---
+++ Config/Db.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\config;
+
+use PDO;
+
+class Db extends PDO {
+
+    private static $instance;
+
+    private function __construct()
+    {
+        $dsn = 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'];
+        parent::__construct($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
+        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+    public static function getInstance(): self 
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
